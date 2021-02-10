4e9ee0a198bf9e6a78f94bb2bf8ab22dfc5bb613
Kutay Yaman <yamankutay1@gmail.com>
2021-02-10 23:11:45 +0300

    Multiple language support has been added to the message that returned when users try to login with the wrong password or e-mail

--- src/main/java/com/kutay/MANPORT/ws/error/ErrorHandler.java
+++ src/main/java/com/kutay/MANPORT/ws/error/ErrorHandler.java
@@ -3,6 +3,7 @@ package com.kutay.MANPORT.ws.error;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.MessageSource;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,19 +17,36 @@ import java.util.Map;
 public class ErrorHandler implements ErrorController {//Eger bizim yakalamadigimiz bir error olursa bunu default olarak Spring kendisi hallediyordu artik oyle bir durum oldugunda buraya gelecek
 
     private final ErrorAttributes errorAttributes;
+    private final MessageSource messageSource;
 
-    public ErrorHandler(ErrorAttributes errorAttributes) {
+    public ErrorHandler(ErrorAttributes errorAttributes, MessageSource messageSource) {
         this.errorAttributes = errorAttributes;
+        this.messageSource = messageSource;
     }
 
     @RequestMapping("/error")
     ApiError handleError(WebRequest webRequest) {
         Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE, ErrorAttributeOptions.Include.BINDING_ERRORS));
+
+        ApiError apiError = createApiError(webRequest, attributes);
+
+        getErrorsToResponse(attributes, apiError);
+
+        return apiError;
+    }
+
+    private ApiError createApiError(WebRequest webRequest, Map<String, Object> attributes) {
         String message = (String) attributes.get("message");
+        if (message.equals("Unauthorized")) {
+            message = messageSource.getMessage("manportapp.warning.unauthorized",null, webRequest.getLocale());
+        }
         String path = (String) attributes.get("path");
         int status = (Integer) attributes.get("status");
         ApiError apiError = new ApiError(status, message, path);
+        return apiError;
+    }
 
+    private void getErrorsToResponse(Map<String, Object> attributes, ApiError apiError) {
         if (attributes.containsKey("errors")) {
             Map<String, String> validationErrors = new HashMap<>();
             List<FieldError> fieldErrors = (List<FieldError>) attributes.get("errors");
@@ -37,9 +55,6 @@ public class ErrorHandler implements ErrorController {//Eger bizim yakalamadigim
             }
             apiError.setValidationErrors(validationErrors);
         }
-
-        return apiError;
-
     }
 
     @Override
