d3b7b7a7f1eb3ab49eca122a2ec725f0fa703d40
Kutay Yaman <yamankutay1@gmail.com>
2021-02-12 15:46:34 +0300

    Multilanguage message added for "User Created" message.

--- src/main/java/com/kutay/MANPORT/ws/service/Impl/UserServiceImpl.java
+++ src/main/java/com/kutay/MANPORT/ws/service/Impl/UserServiceImpl.java
@@ -7,9 +7,13 @@ import com.kutay.MANPORT.ws.service.IUserService;
 import com.kutay.MANPORT.ws.shared.GenericResponse;
 import com.kutay.MANPORT.ws.util.CurrentDateCreator;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 
 @Service
@@ -17,11 +21,13 @@ public class UserServiceImpl implements IUserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final MessageSource messageSource;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder, MessageSource messageSource) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
+        this.messageSource = messageSource;
     }
 
     @Override
@@ -30,7 +36,9 @@ public class UserServiceImpl implements IUserService {
         encodeThePasswordOfUserDTO(userDTO);
         User user = modelMapper.map(userDTO, User.class);
         userRepository.save(user);
-        return ResponseEntity.ok(new GenericResponse("User Created"));
+        Locale locale = LocaleContextHolder.getLocale();
+        String message = messageSource.getMessage("manportapp.warning.userCreated",null, locale);
+        return ResponseEntity.ok(new GenericResponse(message));
     }
 
     private void encodeThePasswordOfUserDTO(UserDTO user) {
