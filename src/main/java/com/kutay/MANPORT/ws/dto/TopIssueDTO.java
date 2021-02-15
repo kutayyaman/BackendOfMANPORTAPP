962f77c1105c7147752cdc38b9ab05c43b539fe7
Kutay Yaman <yamankutay1@gmail.com>
2021-02-16 01:03:10 +0300

    Top3Issues service fixed

--- src/main/java/com/kutay/MANPORT/ws/dto/TopIssueDTO.java
+++ src/main/java/com/kutay/MANPORT/ws/dto/TopIssueDTO.java
@@ -1,6 +1,7 @@
 package com.kutay.MANPORT.ws.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kutay.MANPORT.ws.domain.ImpactType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -13,10 +14,14 @@ public class TopIssueDTO {
     private Long id;
     @ApiModelProperty(required = true,value = "createdDate")
     private String createdDate;
-    @ApiModelProperty(required = true,value = "applicationFullName")
-    private String applicationFullName;
     @ApiModelProperty(required = true,value = "applicationShortName")
     private String applicationShortName;
     @ApiModelProperty(required = true,value = "jobName")
     private String jobName;
+    @ApiModelProperty(required = true,value = "impactType")
+    private String impactType;
+    @ApiModelProperty(required = true,value = "serverName")
+    private String serverName;
+    @ApiModelProperty(required = true,value = "countryName")
+    private String countryName;
 }
--- src/main/java/com/kutay/MANPORT/ws/service/Impl/IssueServiceImpl.java
+++ src/main/java/com/kutay/MANPORT/ws/service/Impl/IssueServiceImpl.java
@@ -1,6 +1,6 @@
 package com.kutay.MANPORT.ws.service.Impl;
 
-import com.kutay.MANPORT.ws.domain.Issue;
+import com.kutay.MANPORT.ws.domain.*;
 import com.kutay.MANPORT.ws.dto.TopIssueDTO;
 import com.kutay.MANPORT.ws.repository.IssueRepository;
 import com.kutay.MANPORT.ws.service.IIssueService;
@@ -26,15 +26,22 @@ public class IssueServiceImpl implements IIssueService {
         return top3TopIssuesToReturn;
     }
 
+    //Burayi kesinlikle refactor etmelisin cunku her get yaptiginda veritabanina sorgu yapiyor mumkun oldugunca az sorgu yollamaya calis
     private List<TopIssueDTO> convertIssueListToTopIssueDTOList(List<Issue> top3IssuesFromDB) {
         List<TopIssueDTO> top3TopIssuesToReturn = new ArrayList<>();
         for (Issue issue : top3IssuesFromDB) {
             TopIssueDTO topIssueDTO = new TopIssueDTO();
             topIssueDTO.setId(issue.getId());
             topIssueDTO.setCreatedDate(issue.getCreatedDate());
-           /* topIssueDTO.setApplicationFullName(issue.getApplication().getFullName());
-            topIssueDTO.setApplicationShortName(issue.getApplication().getShortName());
-            topIssueDTO.setJobName(issue.getJob().getName());*/
+            topIssueDTO.setImpactType(issue.getImpactType().toString());
+            JobImplement jobImplement = issue.getJobImplement();
+            JobInterface jobInterface = jobImplement.getJobInterface();
+            Server server = jobImplement.getServer();
+            Application application = jobInterface.getApplication();
+            topIssueDTO.setCountryName(server.getCountry().getName());
+            topIssueDTO.setApplicationShortName(application.getShortName());
+            topIssueDTO.setJobName(jobInterface.getName());
+            topIssueDTO.setServerName(server.getName());
             top3TopIssuesToReturn.add(topIssueDTO);
         }
         return top3TopIssuesToReturn;
